c5e0e4f96d6b27bd73c4870d2175ead38bce6184
HeloBaron <helo.athayde@gmail.com>
2023-03-09 19:31:30 -0300

    update: Transactions format

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,29 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 import { SummaryContainer, SummeryCard } from "./styles";
 
 export function Summary() {
     const { transactions } = useContext(TransactionsContext);
+
+    const summay = transactions.reduce(
+        (acc, transaction) => {
+            if (transaction.type === 'income') {
+                acc.income += transaction.price;
+                acc.total += transaction.price;
+            } else {
+                acc.outcome += transaction.price;
+                acc.total -= transaction.price;
+            }
+
+            return acc;
+        }, 
+        { income: 0, 
+          outcome: 0, 
+          total: 0
+        }) 
+
     return (
         <SummaryContainer>
             <SummeryCard>
@@ -13,7 +32,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color="#00b37e" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{priceFormatter.format(summay.income)}</strong>
             </SummeryCard>
 
             <SummeryCard>
@@ -22,7 +41,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color="#f75a68" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{priceFormatter.format(summay.outcome)}</strong>
             </SummeryCard>
 
             <SummeryCard variant="green">
@@ -31,7 +50,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color="#fff" />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{priceFormatter.format(summay.total)}</strong>
             </SummeryCard>
         </SummaryContainer>
     )
